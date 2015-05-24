58602109e3c62a0fbf5e59f5559427299328f8c2
Glyph <glyph@twistedmatrix.com>
2015-05-23 17:58:35 -0700

    some docstrings, some debug prints

--- brytly/test/test_stub.py
+++ brytly/test/test_stub.py
@@ -10,21 +10,26 @@ class MyTest(TestCase):
 
     def test_ok(self):
         """
-        
+        This test should pass.
         """
         self.assertEquals(1, 1)
 
     def test_fail(self):
         """
-        
+        This test should fail.
         """
         self.assertEquals(1, 0)
 
     def test_slowly(self):
         """
-        
+        This test should wait for 5 seconds for its Deferred to fire, then
+        pass.
         """
         from browser import window
         d = Deferred()
-        window.setTimeout(lambda: d.callback(None), 5000)
+        print("one")
+        later = lambda: d.callback(None)
+        print("two")
+        window.setTimeout(later, 5000)
+        print("three")
         return d
